5b8a9c00548e405deede9e636b29622bba072413
KB <kbtooeasy@hotmail.fr>
2016-02-24 00:06:40 +0100

    pollut and mean function

--- week 2/Air Pollution/pollutantmean.R
+++ week 2/Air Pollution/pollutantmean.R
@@ -1,14 +1,37 @@
-pollutandmean <- function( pollutant, directory, id = 1:332) {
+
+
+getIndicator <- function(pollutant) {
         
         if (pollutant == "s") 
                 indicator <-  "sulfate"
         else    
                 indicator  <- "nitrate"
-        
-        fileName <- paste(sprintf("%03d", id), ".csv", sep = "")
-        filePath <- paste(directory, fileName, sep="/")
-        rawdataset <- read.csv(filePath)  
-        # cleanDataset <-  na.omit(rawdataset[, indicator])
-        # mean(cleanDataset, na.rm  = TRUE)
+}
 
+geftFileNamesFromId <- function(directory, id) {
+        fileNames <- paste(sprintf("%03d", id), ".csv", sep = "")
+        filePool <- paste(directory, fileNames, sep="/")
+}
+
+
+readMultipleCsvFiles <- function(filePool) {
+        lapply(filePool, function(x) { read.csv(x, header = TRUE)})
+}
+
+bindMultipleDataSet <- function(rawdataset) {
+        Reduce(function(x, y) rbind(x, y), rawdataset)
+}
+
+getColMean <- function(oneDataset, columnName) {
+        mean(oneDataset[ , columnName], na.rm = TRUE)
+}
+
+pollutandmean <- function( pollutant, directory, id = 1:332) {
+        filePool = geftFileNamesFromId(directory, id)
+
+        rawDataset= readMultipleCsvFiles(filePool)
+
+        oneDataSet= bindMultipleDataSet(rawDataset)
+        indicator = getIndicator(pollutant)
+        getColMean(oneDataSet, indicator)
 }
